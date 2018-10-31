edfb575d2160bf9e8eee71dcb1f787cb22d80d88
Serhii Kuts <sergeykuc@gmail.com>
2018-10-31 17:37:32 +0200

    updated with latest code from statechannels

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 'use strict';
 
-var Web3 = require('./lib/web3');
+const Web3Connector = require('./lib/web3');
 
 exports.initialize = function(dataSource, cb) {
-  var settings = dataSource.settings;
-  var connector = new Web3(settings);
-  connector.connect(cb);
-
+  const connector = new Web3Connector(dataSource);
+  connector.dataSource = dataSource;
   dataSource.connector = connector;
+  if (cb) {
+    if (dataSource.settings.lazyConnect) {
+      process.nextTick(function() {
+        cb();
+      });
+    } else {
+      connector.connect(cb);
+    }
+  }
 };
